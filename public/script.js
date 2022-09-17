43d4a8b6d957038f2735812fc56a192fa6bfa567
Trevor Gillespie <trevgillsp@gmail.com>
2022-09-16 18:00:19 -0600

    Added dark mode

--- public/script.js
+++ public/script.js
@@ -3,10 +3,13 @@ const techButton = document.getElementById('tech-button');
 const softButton = document.getElementById('soft-button');
 const techCard = document.getElementById('technical-skills');
 const softCard = document.getElementById('soft-skills');
+const darkButton = document.getElementById('dark-button');
+const body = document.body;
 
 //Event Listeners
 techButton.addEventListener('click', closeSoftCard);
 softButton.addEventListener('click', closeTechCard);
+darkButton.addEventListener('click', toggleBrightness)
 
 //Functions
 function closeSoftCard(event){
@@ -17,5 +20,19 @@ function closeSoftCard(event){
 function closeTechCard(event){
   if (techCard.classList.contains('show')){
     techCard.classList.remove('show');
+  };
+}
+function toggleBrightness(event){
+  if (darkButton.classList.contains('dark')){
+    darkButton.classList.add('light');
+    darkButton.classList.remove('dark');
+    darkButton.innerHTML = '☼';
+    body.classList.add('darkmode');
+
+  } else if (darkButton.classList.contains('light')){
+    darkButton.classList.add('dark');
+    darkButton.classList.remove('light');
+    darkButton.innerHTML = '☽';
+    body.classList.remove('darkmode');
   };
 }
